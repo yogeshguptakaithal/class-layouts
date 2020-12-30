fab782768969ea601184493bef76e9d2bb488198
Aakash-Uchana <42832499+Aakash-Uchana@users.noreply.github.com>
2020-12-31 01:03:38 +0530

    Update 27 Dec - CPM.cpp

--- 27 Dec - CPM.cpp
+++ 27 Dec - CPM.cpp
@@ -27,3 +27,38 @@ public:
 		
 	}
 };
+
+/* Input Format
+n t
+x1 y1 w1
+...
+xt yt wt
+
+1 <= xi, yi <= n
+wi: weight in double
+1 <= i <= t
+*/
+
+/* Sample Input
+Example:
+7 9
+1 2 8
+1 3 2
+2 4 2
+2 5 3
+3 5 2
+4 6 7
+5 6 3
+6 7 6
+5 4 0
+*/
+
+/* Sample Output
+1 0 0
+2 3 3
+3 2 4
+4 6 6
+5 6 6
+6 13 13
+7 19 19
+*/
